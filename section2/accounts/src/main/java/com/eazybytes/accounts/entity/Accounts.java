b9c5a2ab0effc31facd475c37e94cc46c1e0d23a
Sanchez Garcia <olaya.sanchez-garcia@capgemini.com>
2024-08-21 12:25:03 +0200

    Add local changes

---
+++ section2/accounts/src/main/java/com/eazybytes/accounts/entity/Accounts.java
@@ -0,0 +1,25 @@
+package com.eazybytes.accounts.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter @Setter
+@ToString @AllArgsConstructor @NoArgsConstructor
+public class Accounts extends BaseEntity{
+
+    @Id
+    private Long accountNumber;
+
+    private String accountType;
+
+    private String branchAddress;
+
+    private long customerId;
+
+    private String name;
+
+    private String email;
+
+    private String mobileNumber;
+}
---
+++ section2/accounts/src/main/java/com/eazybytes/accounts/service/IAccountsService.java
@@ -0,0 +1,11 @@
+package com.eazybytes.accounts.service;
+
+import com.eazybytes.accounts.dto.CustomerDto;
+
+public interface IAccountsService {
+
+    /**
+     * @param customerDto - CustomerDto Object
+     */
+    void createAccount(CustomerDto customerDto);
+}
