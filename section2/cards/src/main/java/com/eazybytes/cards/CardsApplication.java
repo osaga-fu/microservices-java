66347b626470330ca1bbef5acdb30bb8eda57726
osaga-fu <olaya.saga@gmail.com>
2024-09-24 16:56:26 +0200

    Create cards microservice

---
+++ section2/cards/src/main/java/com/eazybytes/cards/CardsApplication.java
@@ -0,0 +1,39 @@
+package com.eazybytes.cards;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Cards microservice REST API Documentation",
+                description = "Microservice created by Olaya Sánchez",
+                version = "v1",
+                contact = @Contact(
+                        name = "Olaya Sánchez",
+                        email = "olaya@info.com",
+                        url = "https://olaya.info"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "https://www.apache.com"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "Cards microservice REST API external documentation",
+                url = "https://external.ext"
+        )
+)
+public class CardsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardsApplication.class, args);
+    }
+}
