77c336d03284c097598d1db99a13837794bd0b4d
osaga-fu <olaya.saga@gmail.com>
2024-09-24 09:02:33 +0200

    Add validations to Accounts Dto

--- section2/accounts/src/main/java/com/eazybytes/accounts/dto/AccountsDto.java
+++ section2/accounts/src/main/java/com/eazybytes/accounts/dto/AccountsDto.java
@@ -1,14 +1,20 @@
 package com.eazybytes.accounts.dto;
 
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class AccountsDto {
 
+    @NotEmpty(message = "AccountNumber cannot be null or empty")
+    @Pattern(regexp = "(^$|[0-9]{10})", message = "AccountNumber must be 10 digits")
     private Long accountNumber;
 
+    @NotEmpty(message = "AccountType cannot be null or empty")
     private String accountType;
 
+    @NotEmpty(message = "BranchAddress cannot be null or empty")
     private String branchAddress;
 }
